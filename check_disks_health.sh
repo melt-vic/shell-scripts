e8c63f72343e744f82ddf300b4d01b84d9890236
Víctor Iglesias Castán <victorigl@gmail.com>
2026-07-02 10:21:49 +0200

    Cláusula guard añadida. Identación mejorada

--- check_disks_health.sh
+++ check_disks_health.sh
@@ -9,40 +9,41 @@ HOSTNAME=$(hostname)
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 enviar_alerta() {
-   local mensaje="$HOSTNAME tiene la siguiente alerta de disco: $1"
+	local mensaje="$HOSTNAME tiene la siguiente alerta de disco: $1"
 
    # Enviar notificación a Telegram
-    curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
-        -d "chat_id=$CHAT_ID" \
-        --data-urlencode "text=$mensaje" &gt; /dev/null
-}
+   curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
+	   -d "chat_id=$CHAT_ID" \
+	   --data-urlencode "text=$mensaje" > /dev/null
+   }
 
 # sd[a-z]: Linux no mantiene fijo el fichero del disco sino que se lo asigna en función del orden de "llegada" (orden en que "despiertan" los discos)
 for disco in /dev/sd[a-z]; do
-    # ¿Es un fichero de disco?
-    if test -b "$disco" ; then
-        # El flag -n standby evita despertar al disco si está dormido, así se evita desgaste innecesario.
-        LSTR=$(smartctl -H -n standby "$disco" 2&gt;&amp;1)
-        
+	# Si no es un fichero de disco salta al siguiente dispositivo
+	if ! test -b "$disco" ; then
+		continue
+	fi
+	# El flag -n standby evita despertar al disco si está dormido, así se evita desgaste innecesario.
+	LSTR=$(smartctl -H -n standby "$disco" 2>&1)
+
 	# Los dormidos que sigan con Morfeo. El lector de tarjetas vacío (usb bridge) se ignora:
-        if echo "$LSTR" | grep -iqE "Device is in Standby|unknown usb bridge"; then
-            continue
-        fi
-        
-        # Si la salud general no es PASSED, alerta al canto
-        if ! echo "$LSTR" | grep -qE "PASSED|OK"; then
-	    # Extrae modelo y nº de serie del disco
-            IDENTIDAD=$(lsblk -d -n -o MODEL,SERIAL "$disco" | xargs)
-            
-	    # fallback para embellecer el mensaje en el improbable caso de que lsblk no pueda resolver indentidad ni modelo
+	if echo "$LSTR" | grep -iqE "Device is in Standby|unknown usb bridge"; then
+		continue
+	fi
+
+	# Si la salud general no es PASSED, alerta al canto
+	if ! echo "$LSTR" | grep -qE "PASSED|OK"; then
+		# Extrae modelo y nº de serie del disco
+		IDENTIDAD=$(lsblk -d -n -o MODEL,SERIAL "$disco" | xargs)
+
+	    # fallback para embellecer el mensaje en el improbable caso de que lsblk salga con un chorro de babas
 	    if test -z "$IDENTIDAD" ; then
-		   IDENTIDAD="desconocido"
+		    IDENTIDAD="desconocido"
 	    fi
 	    MENSAJE="El disco [$IDENTIDAD] (actualmente $disco) reporta problemas. Output: $LSTR"
 	    enviar_alerta "$MENSAJE"	    
-	    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $MENSAJE" &gt;&gt; $LOG_FILE
-        fi
-    fi
+	    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $MENSAJE" >> $LOG_FILE
+	fi
 done
 
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] Chequeo finalizado." &gt;&gt; $LOG_FILE
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Chequeo finalizado." >> $LOG_FILE
